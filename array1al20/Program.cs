bf7e792ec63a476626c4f7af3fa355250c1f3528
Delvin Villar Arroyo <delvinvillararroyo280398@gmail.com>
2024-09-04 20:04:57 -0600

    Agregar archivos de proyecto.

---
+++ array1al20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace array1al20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numero = new int[20];
+            int i = 0;
+            int contador = 0;
+            for (i = 0; i < numero.Length; i++)
+            {
+                numero[i] = i * 5;
+                Console.WriteLine($" numero: \n{numero[i]}");
+                contador++;
+            }
+        }
+    }
+}
